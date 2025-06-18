3de7feeeeb263287c1918eb38d85b09664769806
HimaniDas <himani.das1998@gmail.com>
2025-06-19 01:35:05 +0600

    Added utils

---
+++ utils/test-data.js
@@ -0,0 +1,35 @@
+module.exports = {
+  users: {
+    standard: {
+      username: 'standard_user',
+      password: 'secret_sauce'
+    },
+    locked: {
+      username: 'locked_out_user',
+      password: 'secret_sauce'
+    },
+    performance: {
+      username: 'performance_glitch_user',
+      password: 'secret_sauce'
+    }
+  },
+  errorMessages: {
+    lockedUser: 'Epic sadface: Sorry, this user has been locked out.'
+  },
+  successMessages: {
+    orderComplete: 'Thank you for your order!'
+  },
+  checkoutInfo: {
+    firstName: 'Himani',
+    lastName: 'Das',
+    postalCode: '12345'
+  },
+  products: [
+    'Sauce Labs Backpack',
+    'Sauce Labs Bike Light',
+    'Sauce Labs Bolt T-Shirt',
+    'Sauce Labs Fleece Jacket',
+    'Sauce Labs Onesie',
+    'Test.allTheThings() T-Shirt (Red)'
+  ]
+};
